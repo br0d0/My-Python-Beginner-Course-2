74b5d2b02a939923dd09b0fc79486fe88b7a27fd
br0d0 <brodier@gmail.com>
2017-02-02 20:51:54 +1100

    Create ex3_input_stdin.py
    pipe input, fileinput, split, strip

---
+++ class1/ex3_input_stdin.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+'''
+3. In the test3.py (from class email week1), how would you modify the script to
+remove the trailing newline on the end of 192.168.1.1?
+
+>>>>> CODE (file test3.py) <<<<<
+#!/usr/bin/env python
+
+import fileinput
+
+for line in fileinput.input():
+    print line.split(".")
+>>>>> END <<<<<
+'''
+
+import fileinput
+
+for line in fileinput.input():
+    line = line.strip()
+    print line.split(".")
